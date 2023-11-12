a41f9b8569da4bca7d3ee131992012f91f878e16
Tarantula1337 <tarantulalp@gmail.com>
2023-11-12 01:19:06 +0100

    Restricting access didn't work, removed for now

--- setupdb.sh
+++ setupdb.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-root_password=M4r1aD8
-
-# Make sure that NOBODY can access the server without a password
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
 
 # Kill the anonymous users
 mysql -e "DROP USER IF EXISTS ''@'localhost'"
