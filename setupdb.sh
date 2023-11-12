b10c8c23ecc4aae5cad9955cbdd7cf1f4dd429db
Tarantula1337 <tarantulalp@gmail.com>
2023-11-12 01:02:02 +0100

    removed sudo from setupdb.sh

--- setupdb.sh
+++ setupdb.sh
@@ -2,11 +2,11 @@
 root_password=M4r1aD8
 
 # Make sure that NOBODY can access the server without a password
-sudo mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
 
 # Kill the anonymous users
-sudo mysql -e "DROP USER IF EXISTS ''@'localhost'"
+mysql -e "DROP USER IF EXISTS ''@'localhost'"
 # Because our hostname varies we'll use some Bash magic here.
-sudo mysql -e "DROP USER IF EXISTS ''@'$(hostname)'"
+mysql -e "DROP USER IF EXISTS ''@'$(hostname)'"
 # Kill off the demo database
-sudo mysql -e "DROP DATABASE IF EXISTS test"
+mysql -e "DROP DATABASE IF EXISTS test"
